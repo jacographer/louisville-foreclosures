b07af8a848a1348ab46054122ebfca715d9767ac
MichaelMcCanless <mjmc273@uky.edu>
2022-11-30 14:49:54 -0500

    zoom-functionality

---
+++ js/app.js
@@ -0,0 +1,22 @@
+(function () {
+
+     // https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/ 
+
+    // 1. need to create onclick.function that takes input from search bar
+     var input = document.getElementById("search-input");
+        
+    // 2. assigns var to that input
+
+    // 3. var is then crossreferenced w/ "PROPERTY" value in point-foreclosure geo.json
+
+    // 4. if "PROPERTY" matches, map zooms to location of "PROPERTY"
+
+    // 5. text column uses dynamic text to change to a template including "PURCHASER", "PURCHASE", and a count of #
+    // of other properties "PURCHASER" has purchased during this time period
+
+    let searchValue = $w("search-input").value;
+
+
+
+
+})();
